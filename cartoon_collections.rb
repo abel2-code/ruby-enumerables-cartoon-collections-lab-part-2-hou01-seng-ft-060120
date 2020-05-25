0ebc1edbfbd0aedd196c26561310c971a63570ce
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-25 23:27:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |element|
-    element[0].upcase + element + "!"
+    element[0].upcase + element.shift + "!"
   end
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
